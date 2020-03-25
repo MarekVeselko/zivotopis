c2fec339b38c14896b1fedeb30a533d3fb1e83e6
MarekVeselko <62647502+MarekVeselko@users.noreply.github.com>
2020-03-25 12:04:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$(".mobil").mouseover(function(){
+  $(".bublina").css("display","block");
+})
+
+$(".mobil").mouseleave(function(){
+  $(".bublina").css("display","none");
+})
+
+$(".mail").mouseover(function(){
+  $(".bublina-mail").css("display","block");
+})
+
+$(".mail").mouseleave(function(){
+  $(".bublina-mail").css("display","none");
+})
